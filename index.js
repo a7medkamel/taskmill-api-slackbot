d603f084bc3ae401505c82d971dd555c6c7fbd33
Ahmed Kamel <ak@ahmedkamel.net>
2016-08-09 17:45:54 -0700

    SlackBot client-proxy prototype

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var Bot      = require('./lib/bot')
+  , BotProxy = require('./lib/proxy')
+  , _        = require('lodash')
+  ;
+
+module.exports = {
+    Bot       : Bot
+  , BotProxy  : BotProxy
+};
